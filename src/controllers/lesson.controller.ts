d86395c2d64aa8ab86c4ba01c3a53740d695d898
unknown <nguyennhuphuoc2002@gmail.com>
2024-04-09 21:22:03 +0700

    feat: api get lessons

--- src/controllers/lesson.controller.ts
+++ src/controllers/lesson.controller.ts
@@ -19,7 +19,29 @@ async function getAllChapter(req: Request, res: Response): Promise<any> {
     data: chapters
   });
 }
+async function getVideoByLessonId(req: Request, res: Response): Promise<any> {
+  const { lessonId } = req.body;
+
+  const video = await lessonService.getVideoByLessonId(lessonId);
+
+  return res.status(200).json({
+    message: video ? `Video is found` : `Video is not found`,
+    data: video
+  });
+}
+async function getBookByLessonId(req: Request, res: Response): Promise<any> {
+  const { lessonId } = req.body;
+
+  const book = await lessonService.getBookByLessonId(lessonId);
+
+  return res.status(200).json({
+    message: book ? `Book is found` : `Book is not found`,
+    data: book
+  });
+}
 export default {
     getAllChapter,
-    getLessonByChapter
+    getLessonByChapter,
+    getBookByLessonId,
+    getVideoByLessonId
 }
