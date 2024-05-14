103f95a7dc1793e4ef82d8d780507f326f5d44e6
hiepnt <thaihiep232002@gmail.com>
2024-05-14 16:39:45 +0700

    fix: hotfix

--- src/entities/userAccount.entity.ts
+++ src/entities/userAccount.entity.ts
@@ -5,8 +5,6 @@ export interface UserAccountEntity {
     eventpoint: number;
     star: number;
     coin: number;
-    skin_id: number;
-    frame_id: number;
     created_at: Date;
     updated_at: Date;
     skin_id: number | null;
